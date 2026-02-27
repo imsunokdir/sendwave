db239fb9640387145841330b60f9cdf724d6964a
imsunokdir <tangitnokdir40@gmail.com>
2026-02-27 12:15:42 +0530

    local redis + docker setup

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,13 +1,8 @@
 import { ConnectionOptions } from "bullmq";
-require("dotenv").config();
 
 const redisConnection: ConnectionOptions = {
-  host: new URL(process.env.UPSTASH_REDIS_REST_URL as string).hostname,
+  host: "localhost",
   port: 6379,
-  password: process.env.UPSTASH_REDIS_REST_TOKEN,
-  tls: {
-    rejectUnauthorized: false,
-  },
 };
 
 export default redisConnection;
