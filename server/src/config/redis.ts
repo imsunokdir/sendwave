a18113d9542d4fa8fca7cd3e7f1d16be359d997c
imsunokdir <tangitnokdir40@gmail.com>
2026-02-27 13:09:51 +0530

    local redis + docker setup

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,15 +1,12 @@
-import { ConnectionOptions } from "bullmq";
+import IORedis from "ioredis";
 
-const redisConnection: ConnectionOptions = process.env.REDIS_URL
-  ? {
-      url: process.env.REDIS_URL,
-      tls: {
-        rejectUnauthorized: false,
-      },
-    }
-  : {
-      host: "localhost",
+const redisConnection = process.env.REDIS_URL
+  ? new IORedis(process.env.REDIS_URL, {
+      tls: { rejectUnauthorized: false },
+    })
+  : new IORedis({
+      host: "127.0.0.1",
       port: 6379,
-    };
+    });
 
 export default redisConnection;
