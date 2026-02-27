cac07b88c65b7b78d6b7404b50221a901c0318f2
imsunokdir <tangitnokdir40@gmail.com>
2026-02-27 13:33:03 +0530

    fix redis connection for bullmq

--- server/src/config/redis.ts
+++ server/src/config/redis.ts
@@ -1,14 +1,13 @@
-import IORedis from "ioredis";
-require("dotenv").config();
+import { ConnectionOptions } from "bullmq";
 
-// Create Redis connection
-const redisConnection = process.env.REDIS_URL
-  ? new IORedis(process.env.REDIS_URL, {
+const redisConnection: ConnectionOptions = process.env.REDIS_URL
+  ? {
+      url: process.env.REDIS_URL,
       tls: { rejectUnauthorized: false },
-    })
-  : new IORedis({
+    }
+  : {
       host: "127.0.0.1",
       port: 6379,
-    });
+    };
 
 export default redisConnection;
